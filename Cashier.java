a7edd2209904a9e09322b32b155f9c3476b99012
Gabriel <germanseifeg@yahoo.com>
2019-06-24 09:38:02 -0400

    Test

---
+++ Cashier.java
@@ -0,0 +1,21 @@
+package TimeSimulation;
+
+public class Cashier {
+	private Costumer aCostumer;
+	// Queue type Customer goes here
+	
+	public void addCustomer(Customer customer) {
+		
+	} //end addCustomer
+	
+	public void removeCustomer() {
+		
+	} //end removeCustomer
+	
+	public void serviceCostumer(Costumer costumer) {
+		/* Gets the the service required by customer and
+		dequeues customer when finishes
+		*/
+	}//end servceCustomer
+
+}
