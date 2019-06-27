8066b0b029761dc73b0d36f8268244f621361066
Matthew Bailey <mattymosh580@gmail.com>
2019-06-27 10:36:50 -0400

    changed isComplete

--- Customer.java
+++ Customer.java
@@ -3,6 +3,7 @@ public class Customer {
     private double arrivalTime;
     private double serviceTime; 
 
+
     private Customer(double arrTime, double svcTime) {
         this.arrivalTime = arrTime;
         this.serviceTime = svcTime;
@@ -12,15 +13,9 @@ public class Customer {
         serviceTime -= serviceTime;
     }
 
-    private  isComplete() {
-        if (serviceTime == 0){
-            return true;
-        }
-        else{
-            return false;
-        }
+    public  isComplete() {
+        return serviceTime == 0;
     }
-
 
     public static Customer generateCustomer(double arrTime, double svcTime){
         return new Customer(arrTime, svcTime);
