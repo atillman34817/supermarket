e50a6f07e3ff61f1b2e09138631a1dbd8b0deed1
Matthew Bailey <mattymosh580@gmail.com>
2019-06-27 10:20:19 -0400

    added a takeService and added a isComplete

--- Customer.java
+++ Customer.java
@@ -1,11 +1,26 @@
 public class Customer {
+
     private double arrivalTime;
-    private double serviceTime;
+    private double serviceTime; 
 
     private Customer(double arrTime, double svcTime) {
         this.arrivalTime = arrTime;
         this.serviceTime = svcTime;
     }
+
+    public takeService(){
+        serviceTime -= serviceTime;
+    }
+
+    private  isComplete() {
+        if (serviceTime == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
 
     public static Customer generateCustomer(double arrTime, double svcTime){
         return new Customer(arrTime, svcTime);
